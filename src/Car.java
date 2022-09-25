f96f6eb12c258bd236d051c93efecb12605fbba8
serge <clonsfer@gmail.com>
2022-09-25 12:04:05 +0300

    Выполненное ДЗ

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,8 @@
-public class Car {
+public class Car extends Transport{
 
-    public String modelName;
-    public int wheelsCount;
+    public Car(String modelName, int wheelsCount){
+        super(modelName, wheelsCount);
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
